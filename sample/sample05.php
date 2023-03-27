2bfcfb693da8af356ca0f67b2a0b3e4d26a43431
dkseho9121 <dkseho9121@gmail.com>
2023-03-27 17:36:37 +0900

    2023.03.27

---
+++ sample/sample05.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://webfontworld.github.io/ChosunGs/ChosunGs.css" rel="stylesheet">
+
+    <link href="css/style.css" rel="stylesheet">
+    
+</head>
+<body>
+    <main>
+        <aside>
+            <?php
+                include "aside.php";
+            ?>
+        </aside>
+        <section>
+            <?php
+                include "../connect/connect.php";
+
+            ?>
+        </section>
+    </main>
+</body>
+</html>
